760be5b76b84956157ff588eea0bd0725699dc29
santosfk <santosguilhermefk@gmail.com>
2022-03-14 23:32:21 -0300

    creating application tokens

---
+++ src/theme/index.js
@@ -0,0 +1,16 @@
+import { Roboto_500Medium } from "@expo-google-fonts/roboto";
+
+export default {
+  COLOR: {
+    BACKGROUND_900: "#40012A",
+    BACKGROUND_800: "#F23064",
+    BACKGROUND_700: "#F27798",
+    TEXT: "#F2F2F2",
+    ICONS: "#F29F05",
+  },
+  FONT: {
+    REGULAR: "Roboto_400Regular",
+    MEDIUM: "Roboto_500Medium",
+    BOLD: "Roboto_700Bold",
+  },
+};
