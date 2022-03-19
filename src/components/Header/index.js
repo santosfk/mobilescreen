3cf776d38f0339edc0a8d2c50f0659cfea2d5e74
santosfk <santosguilhermefk@gmail.com>
2022-03-19 20:35:56 -0300

    remaking style and data of the header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,8 +1,15 @@
 import React from "react";
-import { Container } from "./style";
+import { Container, ImageUser, NameUser, LevelUser } from "./style";
+import ManImg from "../../assets/Homem.jpg";
 import { createMaterialBottomTabNavigator } from "@react-navigation/material-bottom-tabs";
 
 export default function Header() {
   const Tab = createMaterialBottomTabNavigator();
-  return <Container></Container>;
+  return (
+    <Container>
+      <ImageUser source={ManImg} />
+      <NameUser>José Adolfo</NameUser>
+      <LevelUser>{`lv ${26}`}</LevelUser>
+    </Container>
+  );
 }
--- src/components/Header/style.js
+++ src/components/Header/style.js
@@ -1,18 +1,28 @@
 import styled from "styled-components/native";
-
+import { RFPercentage } from "react-native-responsive-fontsize";
 export const Container = styled.View`
   width: 100%;
   height: 60px;
   background-color: ${({ theme }) => theme.COLOR.BACKGROUND_800};
   box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
   position: absolute;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
   z-index: 2;
 `;
-export const MenuItems = styled.View`
-  margin: auto;
-  color: white;
-  flex-direction: row;
+export const ImageUser = styled.Image`
+  height: ${RFPercentage(7)}px;
+  width: ${RFPercentage(7)}px;
+  border-radius: 50%;
 `;
-export const Item = styled.View`
-  margin: 0px 30px;
+export const NameUser = styled.Text`
+  color: ${({ theme }) => theme.COLOR.TEXT};
+  font-family: ${({ theme }) => theme.FONT.BOLD};
+  font-size: ${RFPercentage(3)}px;
+`;
+export const LevelUser = styled.Text`
+  font-size: ${RFPercentage(3)}px;
+  color: ${({ theme }) => theme.COLOR.TEXT};
+  font-family: ${({ theme }) => theme.FONT.BOLD};
 `;
