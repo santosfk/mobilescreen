68a75e25fde591ab2701d450ab19ad166e0aa2c2
santosfk <santosguilhermefk@gmail.com>
2022-03-21 19:41:30 -0300

    implementing new color tokens, themed dark and light

---
+++ src/themes/dark.js
@@ -0,0 +1,15 @@
+export default {
+  COLOR: {
+    BACKGROUND: "#111111",
+    BACKGROUND_900: "#40012A",
+    BACKGROUND_800: "#F23064",
+    BACKGROUND_700: "#F27798",
+    TEXT: "#F2F2F2",
+    ICONS: "#F29F05",
+  },
+  FONT: {
+    REGULAR: "Roboto_400Regular",
+    MEDIUM: "Roboto_500Medium",
+    BOLD: "Roboto_700Bold",
+  },
+};
---
+++ src/themes/light.js
@@ -0,0 +1,15 @@
+export default {
+  COLOR: {
+    BACKGROUND: "#F2F2F2",
+    BACKGROUND_900: "#40012A",
+    BACKGROUND_800: "#F23064",
+    BACKGROUND_700: "#F27798",
+    TEXT: "#111111",
+    ICONS: "#F29F05",
+  },
+  FONT: {
+    REGULAR: "Roboto_400Regular",
+    MEDIUM: "Roboto_500Medium",
+    BOLD: "Roboto_700Bold",
+  },
+};
